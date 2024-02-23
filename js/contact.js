9df774e2917c85983558312e42ae38436389e0b7
Salcedin <billy.talent01@gmail.com>
2024-02-23 22:19:08 +0100

    Cambios

--- js/contact.js
+++ js/contact.js
@@ -1,19 +1,32 @@
 /**
  * Código JS para la funcionalidad del formulario de contacto.
- * Maneja el envío del formulario y valida los campos antes de enviarlos
+ * Maneja el envío del formulario y valida los campos antes de enviarlos.
  */
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    /* Obtiene el formulario */
+    /**
+     * Elemento del formulario de contacto.
+     * @type {HTMLFormElement}
+     */
     const contactForm = document.getElementById('contactForm');
 
-    /* Agrega un sumbit al formulario */
+    /**
+     * Maneja el evento de envío del formulario.
+     * @param {Event} e - El evento de envío del formulario.
+     */
     contactForm.addEventListener('submit', (e) => {
         e.preventDefault(); // Previene la recarga de la página
 
-        // Obtiene los valores del formulario
+        /**
+         * Valor del campo del nombre obtenido del formulario.
+         * @type {string}
+         */
         const name = document.getElementById('name').value;
+
+        /**
+         * Valor del campo del mensaje obtenido del formulario.
+         * @type {string}
+         */
         const message = document.getElementById('message').value;
 
         // Verifica si el nombre y el mensaje no están vacíos
@@ -21,8 +34,12 @@ document.addEventListener('DOMContentLoaded', () => {
             alert(`¡Gracias por tu mensaje, ${name}!`);
             contactForm.reset(); // Resetea el formulario después del envío
         } else {
-            /* Si el nombre o el mensaje están vacíos, muestra un alert */
-            alert('Por favor, completa todos los campos.');
+            /**
+             * Mensaje de alerta si el nombre o el mensaje están vacíos.
+             * @type {string}
+             */
+            const errorMessage = 'Por favor, completa todos los campos.';
+            alert(errorMessage);
         }
     });
 });
